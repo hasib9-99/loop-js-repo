9adb6c2b3478f0e2b1771aff30d015dde00e6cf0
hasib9-99 <abuhasibg@gmail.com>
2022-11-08 20:09:35 +0600

    make a loop

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+for(i = 1; i <= 20; i++){
+    for(j = 1; j <= 20; j ++){
+        let ruselt = i * j
+        console.log(ruselt);
+        document.write(i + " x " + j + " = " + ruselt + "</br>")
+    }
+}
