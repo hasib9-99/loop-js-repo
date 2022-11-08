f25e7b92ba7d24a3d4f2bd38b39da50b4b17d82c
hasib9-99 <abuhasibg@gmail.com>
2022-11-08 20:17:53 +0600

    use for loop

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,9 @@
 for(i = 1; i <= 20; i++){
+    document.write("<div class='one'>")
     for(j = 1; j <= 20; j ++){
         let ruselt = i * j
         console.log(ruselt);
-        document.write(i + " x " + j + " = " + ruselt + "</br>")
+        document.write(i + " x " + j + " = " + ruselt + "</br>")    
     }
+    document.write("</div>")
 }
